ef4e1b4378b841294b568f3720abd59e58fa5ad9
BuildTools <unconfigured@null.spigotmc.org>
2020-05-28 00:25:30 +0200

    Add file reading methods

--- Config/src/me/duco/Main.java
+++ Config/src/me/duco/Main.java
@@ -3,12 +3,18 @@ package me.duco;
 import me.duco.file.FileHandler;
 import me.duco.mqtt.MqttHandler;
 
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] arg) {
+        Path homedir = Paths.get(System.getProperty("user.home"), "SolidMQ");
 
-        FileHandler files = new FileHandler();
-        MqttHandler client = new MqttHandler();
+        FileHandler files = new FileHandler(homedir.toString(), "/new.txt");
+        files.appendLog("Fromage", "\t");
+        //MqttHandler client = new MqttHandler();
 
     }
 }
--- Config/src/me/duco/file/FileHandler.java
+++ Config/src/me/duco/file/FileHandler.java
@@ -1,5 +1,73 @@
 package me.duco.file;
 
-public class FileHandler {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FileHandler {
+    private String installpath, filename;
+    private File file;
+
+    public FileHandler(String path, String name) {
+        installpath = path;
+        filename = name;
+        emptyLog();
+    }
+
+    public void emptyLog() {
+        file = new File(installpath + filename);
+        try {
+            if(!file.exists()) {
+                if(new File(installpath).mkdirs()) { //Create dirs if no existant
+                    System.out.println("Created directory: " + installpath);
+                }
+            }
+
+            if(file.createNewFile()) {
+                System.out.println("Created file: " + installpath + filename);
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred:");
+            e.printStackTrace();
+        }
+    }
+
+    public void writelog() { // Will overwrite existing file
+        try {
+            FileWriter out = new FileWriter(installpath + filename);
+            out.write("Written from class");
+            out.close();
+
+            System.out.println("Written to file");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred:");
+            e.printStackTrace();
+        }
+    }
+
+    public void appendLog(String what, String endline) { // Will append to file
+        try {
+            FileWriter out = new FileWriter(installpath  + filename, true);
+            out.append(what).append(endline);
+            out.close();
+
+            System.out.println("Appended to file");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred:");
+            e.printStackTrace();
+        }
+    }
+
+    public void appendLog(String what) { // Overload appendLog method to allow default parameter.
+        appendLog(what, "\t");
+    }
+
+    public String readLog() {
+
+        //ToDo implement log reading
+        return null;
+    }
 }
